3a68468a8e7d56f00cc7adcb0787e7a9264286b7
teja123143 <mula.ravitejareddy@gmail.com>
2019-12-06 23:24:42 +0530

    this is night project

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("Hello, Naresh");
+    System.out.println("Hello, Suresh");
+    System.out.println("Hello, Ramesh");
+    System.out.println("Hello, Pratap");
+    System.out.println("Hello, Jana");
+    System.out.println("Hello, secondline");
+    System.out.println("Hello, Banglore good morning");
+    System.out.println("Hello, Marathahalli");
+    System.out.println("Hello, SGR dental college road");
+    System.out.println("Hello, Banglore guys")
+  }
+}
